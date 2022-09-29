da4982e7991941500921392b229d68d552b67754
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-29 16:29:55 +0200

    finito da sistemare css

--- script/script.js
+++ script/script.js
@@ -1,10 +1,54 @@
+const items = document.getElementById("items");
 function getInputAndAddItem(event) {
   if (event.key !== "Enter") {
     return;
   }
   let input = document.getElementById("input-item");
   if (input.value !== "") {
-    items.innerHTML += `<div class="item" onclick="selectItem(event)">${input.value}</div>`;
+    items.innerHTML += `<div class="item">${input.value}</div>`;
+    input.value = "";
+  } else {
+    alert("Il campo di testo è vuoto!");
+  }
+}
+
+function invio() {
+  let input = document.getElementById("input-item");
+  if (input.value !== "") {
+    items.innerHTML += `<div class="item">${input.value}</div>`;
+    input.value = "";
+  } else {
+    alert("Il campo di testo è vuoto!");
+  }
+}
+
+function maiuscolo() {
+  let up = document.getElementById("input-item");
+  let input = document.getElementById("input-item");
+  if (up.value !== "") {
+    items.innerHTML += `<div class="item upper">${input.value}</div>`;
+    input.value = "";
+  } else {
+    alert("Il campo di testo è vuoto!");
+  }
+}
+
+function grassetto() {
+  let up = document.getElementById("input-item");
+  let input = document.getElementById("input-item");
+  if (up.value !== "") {
+    items.innerHTML += `<div class="item bold">${input.value}</div>`;
+    input.value = "";
+  } else {
+    alert("Il campo di testo è vuoto!");
+  }
+}
+
+function sottolinea() {
+  let up = document.getElementById("input-item");
+  let input = document.getElementById("input-item");
+  if (up.value !== "") {
+    items.innerHTML += `<div class="item underline">${input.value}</div>`;
     input.value = "";
   } else {
     alert("Il campo di testo è vuoto!");
